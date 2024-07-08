4dd9431508e5b59390ec3e357461338e30da30d8
Carlos Herrera <carlos.herrera@materiagris.pe>
2024-07-08 17:51:38 -0500

    integrate api

--- src/app/shadow/shadow-user/shadow-user.component.ts
+++ src/app/shadow/shadow-user/shadow-user.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/shared/shared.module';
-
+import { Serviceservice, ItemApi } from 'src/service/service.service';
 
 @Component({
   selector: 'app-shadow-user',
   standalone: true,
   templateUrl: './shadow-user.component.html',
   styleUrls: ['./shadow-user.component.css'],
-  imports: [SharedModule]
+  imports: [SharedModule, CommonModule]
 })
 export class ShadowUserComponent {
 
+  element: ItemApi[] = [];
+
+  constructor(private apiService: Serviceservice) {}
+
+  ngOnInit() {
+    this.GetData();
+  }
+  
+  GetData() {
+    this.apiService.getObjects().subscribe(
+      (data) => {
+        this.element = data;
+        console.log(this.element);
+      },
+      (error) => {
+        console.error('Error fetching tasks:', error);
+      }
+    );
+  }
 }
--- src/service/service.service.ts
+++ src/service/service.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface ItemApi {
+  id: number;
+  objectId: number;
+  name: string;
+  description: string;
+  customer: string;
+  price: number;
+  stock: number;
+  img: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ServiceService {
+export class Serviceservice {
+  private baseUrl = 'http://localhost:5146/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getObjects(data?: any): Observable<any> {
+    return this.http.get<any[]>(`${this.baseUrl}/ObjectsEcommerces`);
+  }
+
+  createObject(data?: any): Observable<any> {
+    return this.http.post<any[]>(`${this.baseUrl}/ObjectsEcommerces`, data);
+  }
+
+  deleteObject(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/ObjectsEcommerces/${id}`);
+  }
+
+  updateObject(Object: any): Observable<any> {
+    return this.http.put(
+      `${this.baseUrl}/ObjectsEcommerces/${Object.id}`,
+      Object
+    );
+  }
 }
